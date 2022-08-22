d516d1c518b388bc595ca03c8796c4108721ce10
trafterian <99242625+Trafterian@users.noreply.github.com>
2022-08-22 10:29:05 +0300

    Monitor transactions by HTTP

--- src/bin/eth_tx_monitor.ts
+++ src/bin/eth_tx_monitor.ts
@@ -1,28 +1,35 @@
 import Web3 from 'web3';
+import dayjs from "dayjs";
 import dotenv from 'dotenv'
 import {AbiItem} from 'web3-utils';
-import {EventData} from 'web3-eth-contract';
 import HashedTimelockAbi from '../assets/abi/HashedTimelock.json'
 import {CreateDeposit, createDepositHandler} from '../../Context/Eth';
 import '../../Context'
 
 dotenv.config()
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider(
-    `wss://${process.env.ETH_NETWORK_NAME}.infura.io/ws/v3/${process.env.INFURA_API_KEY}`
+const fromBlock = process.env.DEPLOY_CONTRACT_BLOCK
+
+const web3 = new Web3(new Web3.providers.HttpProvider(
+    `https://${process.env.ETH_NETWORK_NAME}.infura.io/v3/${process.env.INFURA_API_KEY}`
 ))
 const contract = new web3.eth.Contract(HashedTimelockAbi as AbiItem[], process.env.ETH_CONTRACT_ADDRESS)
 
-contract.events.LogHTLCNew({
-    fromBlock: process.env.DEPLOY_CONTRACT_BLOCK,
-    topics: [process.env.ETH_CONTRACT_TOPIC_HTLC_NEW]
-})
-    .on('connected', function (subscriptionId: string) {
-        console.log('Start watching new HTLC contracts');
-    })
-    .on('data', function (event: EventData) {
-        console.log(`Found new contract ${event.returnValues.contractId}`);
-        
+const processEvents = async () => {
+    const toBlock = (await web3.eth.getBlockNumber()) - parseInt(process.env.ETH_BLOCK_CONFIRMATION_NUMBER as string, 10)
+
+    const events = await contract.getPastEvents(
+        'LogHTLCNew',
+        {
+            fromBlock,
+            toBlock
+        }
+    )
+
+    console.log(`LogHTLCNew: ${dayjs().format()}. From ${fromBlock} to ${toBlock} blocks`);
+    console.log(`Found ${events.length} new events`);
+
+    for (const event of events) {
         const command = new CreateDeposit(
             event.transactionHash,
             event.returnValues.contractId,
@@ -33,12 +40,16 @@ contract.events.LogHTLCNew({
             event.returnValues.timelock,
         )
 
-        createDepositHandler.execute(command)
-    })
-    .on('changed', function (event: EventData) {
-        // Could this happen? And when?
-    })
-    .on('error', function (error: any) {
-        console.log('error', error);
-    });
+        const result = await createDepositHandler.execute(command)
+
+        if (result.isLeft()) {
+            console.log(`Error while proccessed transaction ${event.transactionHash}: `, result.value.error?.message)
+        }
+    }
+}
+
+processEvents().then(() => {
+    console.log('LogHTLCNew: Finished');
+    process.exit()
+})
 
