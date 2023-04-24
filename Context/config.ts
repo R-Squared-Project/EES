bb8308e480abae69a036768767d621623bcc52f9
Griffics <126185286+Griffics@users.noreply.github.com>
2023-04-24 11:09:37 +0300

    add new settings to SettingsController

--- Context/config.ts
+++ Context/config.ts
@@ -11,10 +11,10 @@ const config = {
     eth: {
         providers: {
             infura: {
-                api_key: process.env.INFURA_API_KEY
-            }
+                api_key: process.env.INFURA_API_KEY,
+            },
         },
-        provider: 'infura',
+        provider: "infura",
         network: process.env.ETH_NETWORK_NAME as string,
         private_key: process.env.ETH_PRIVATE_KEY as string,
         minimum_deposit_amount: Web3.utils.toBN(Web3.utils.toWei(process.env.MINIMUM_DEPOSIT_AMOUNT as string)),
@@ -32,20 +32,20 @@ const config = {
         redeem_timeframe: parseInt(process.env.TIMEFRAME_REDEEM_INTERNAL_BLOCKCHAIN as string, 10),
         chain_id: process.env.REVPOP_CHAIN_ID,
         rvp_withdrawal_fee: parseFloat(process.env.REVPOP_RVP_WITHDRAWAL_FEE as string),
-        rveth_withdrawal_fee: parseFloat(process.env.REVPOP_RVETH_WITHDRAWAL_FEE as string)
-        eth_to_rveth_rate: parseFloat(process.env.REVPOP_ETH_TO_RVETH_RATE ?? "1")
+        rveth_withdrawal_fee: parseFloat(process.env.REVPOP_RVETH_WITHDRAWAL_FEE as string),
+        eth_to_rveth_rate: parseFloat(process.env.REVPOP_ETH_TO_RVETH_RATE ?? "1"),
     },
     db: {
         name: process.env.DATABASE,
         host: process.env.DATABASE_HOST,
         port: parseInt(process.env.DATABASE_PORT as string, 10),
         user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD
+        password: process.env.DATABASE_PASSWORD,
     },
     rabbitmq: {
         host: process.env.RABBITMQ_HOST as string,
-        port: parseInt(process.env.RABBITMQ_PORT as string, 10)
-    }
-}
+        port: parseInt(process.env.RABBITMQ_PORT as string, 10),
+    },
+};
 
-export default config
+export default config;
--- src/Wallet/GetSettingsController.ts
+++ src/Wallet/GetSettingsController.ts
@@ -2,23 +2,23 @@ import { Controller, Get, HttpCode } from "@nestjs/common";
 import SuccessResponse from "../Response/SuccessResponse";
 import config from "context/config";
 
-@Controller("deposit")
+@Controller("settings")
 export default class GetSettingsController {
-  @Get("settings")
-  @HttpCode(200)
-  async create(): Promise<SuccessResponse> {
-    return Promise.resolve(
-      SuccessResponse.create({
-        contract_address: config.eth.contract_address,
-        receiver_address: config.eth.receiver,
-        minimum_deposit: config.eth.minimum_deposit_amount.toString(),
-        minimum_timelock: config.contract.minimum_timelock,
-        withdraw_timelock: config.contract.withdrawn_timelock,
-        rvp_withdrawal_fee: config.revpop.rvp_withdrawal_fee,
-        rveth_withdrawal_fee: config.revpop.rveth_withdrawal_fee,
-        revpop_asset_symbol: config.revpop.asset_symbol,
-        revpop_ees_account: config.revpop.ees_account,
-      })
-    );
-  }
+    @Get("")
+    @HttpCode(200)
+    async create(): Promise<SuccessResponse> {
+        return Promise.resolve(
+            SuccessResponse.create({
+                contract_address: config.eth.contract_address,
+                receiver_address: config.eth.receiver,
+                minimum_deposit: config.eth.minimum_deposit_amount.toString(),
+                minimum_timelock: config.contract.minimum_timelock,
+                withdraw_timelock: config.contract.withdrawn_timelock,
+                rvp_withdrawal_fee: config.revpop.rvp_withdrawal_fee,
+                rveth_withdrawal_fee: config.revpop.rveth_withdrawal_fee,
+                revpop_asset_symbol: config.revpop.asset_symbol,
+                revpop_ees_account: config.revpop.ees_account,
+            })
+        );
+    }
 }
