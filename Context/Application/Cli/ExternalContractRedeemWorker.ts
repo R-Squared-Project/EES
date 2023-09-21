5da83d460e7f44d124fd22857c214f4a80c67643
Griffics <126185286+Griffics@users.noreply.github.com>
2023-09-21 11:36:49 +0300

    unit test ExternalWithdrawRedeem, added StubQueue

--- Context/Application/Cli/ExternalContractRedeemWorker.ts
+++ Context/Application/Cli/ExternalContractRedeemWorker.ts
@@ -2,6 +2,8 @@ import { Command, CommandRunner } from "nest-commander";
 import RabbitMQ from "context/Queue/RabbitMQ";
 import ConfirmDepositExternalContractRedeemed from "context/Application/Command/ExternalBlockchain/ConfirmDepositExternalContractRedeemed/ConfirmDepositExternalContractRedeemed";
 import ConfirmDepositExternalContractRedeemedHandler from "context/Application/Command/ExternalBlockchain/ConfirmDepositExternalContractRedeemed/ConfirmDepositExternalContractRedeemedHandler";
+import QueueInterface, { EXTERNAL_DEPOSIT_CONTRACT_REDEEM } from "context/Queue/QueueInterface";
+import { Inject } from "@nestjs/common";
 
 interface ExternalContractRedeemMessage {
     txHash: string;
@@ -12,13 +14,16 @@ interface ExternalContractRedeemMessage {
     description: "Consume RabbitMQ message to confirm deposit external contract redeemed",
 })
 export class ExternalContractRedeemWorker extends CommandRunner {
-    constructor(private queue: RabbitMQ, private handler: ConfirmDepositExternalContractRedeemedHandler) {
+    constructor(
+        @Inject("QueueInterface") private queue: QueueInterface,
+        private handler: ConfirmDepositExternalContractRedeemedHandler
+    ) {
         super();
     }
 
     async run(): Promise<void> {
         this.queue.consume<ExternalContractRedeemMessage>(
-            this.queue.EXTERNAL_DEPOSIT_CONTRACT_REDEEM,
+            EXTERNAL_DEPOSIT_CONTRACT_REDEEM,
             async (message: ExternalContractRedeemMessage, ack, nack) => {
                 const command = new ConfirmDepositExternalContractRedeemed(message.txHash);
 
